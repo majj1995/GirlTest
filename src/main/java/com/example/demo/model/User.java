1cc9450a4fe606f5056e19fa08d6ddb8a5a39a70
马佳君 <595641927@qq.com>
2018-11-07 17:06:22 +0800

    version2

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,24 @@
+package com.example.demo.model;
+
+public class User {
+    private Integer id;
+    private String company;
+
+    public User() {}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+}
